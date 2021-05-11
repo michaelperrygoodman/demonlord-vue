857cba16ff021a5c51b0830503814ac92922f078
Michael Perry Goodman <michaelperrygoodman@gmail.com>
2021-05-11 10:09:43 -0500

    remove unused nav items

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,7 +1,5 @@
 <template>
   <ul class="main-menu">
     <li><router-link to="/resume">/resume</router-link></li>
-    <li><router-link to="/warez">/warez</router-link></li>
-    <li><router-link to="/etc">/etc</router-link></li>
   </ul> <!-- /nav -->
 </template>
